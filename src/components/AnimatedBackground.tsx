3a6cfc919335401cae15222a5f2a9eddfaac0b3b
Ayush Kumar Jha <jha44481@gmail.com>
2026-01-19 22:24:50 +0530

    fix: update AnimatedBackground to import css module

--- src/components/AnimatedBackground.tsx
+++ src/components/AnimatedBackground.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import './AnimatedBackground.css';
+import styles from './AnimatedBackground.module.css';
 
 const AnimatedBackground: React.FC = () => {
-    // Create an array of 21 items (0 to 20) as per the HTML
+    // Generates 21 items to match the user's HTML structure:
+    // <div class="item" style="--i:0;"></div> ... up to 20
     const items = Array.from({ length: 21 }, (_, i) => i);
 
     return (
-        <div className="animation-wrapper">
-            <div className="animation-container">
+        <div className="fixed inset-0 pointer-events-none z-0 overflow-hidden" aria-hidden="true">
+            <div className={styles.container}>
                 {items.map((i) => (
-                    <div key={i} className="item" style={{ '--i': i } as React.CSSProperties}></div>
+                    <div
+                        key={i}
+                        className={styles.item}
+                        style={{ '--i': i } as React.CSSProperties}
+                    ></div>
                 ))}
             </div>
         </div>
